b2c650f947431782cd3ad0fb28b58ba14f1e68a5
Thomas <thomas.galocha@hotmail.fr>
2025-03-31 11:58:20 +0200

    update vite config

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,10 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    server: {
+        allowedHosts: true,
+    },
+  });
+};
